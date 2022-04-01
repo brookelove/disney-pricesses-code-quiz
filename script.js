fe347256469f03253c0c81bff675c7c0f299e581
Brooke Love <savienloveb@gmail.com>
2022-03-31 21:28:21 -0700

    uploading js pseudocode

---
+++ script.js
@@ -0,0 +1,23 @@
+//PSEUDOCODE
+    //select all HTML elements 
+        //select all of div elementrs 
+        //select all id elements
+    //global question index @ 0 
+    //global timer  how much time (assign in id: undefined)
+    //start fucntion to start quiz 
+        //start timer
+        //call a function to get question 
+    //function to get question 
+        //access the question @ the current index 
+        //display question in html 
+        //display answer (loop)
+            //assign them a click handler "when click happens x happens"
+    //function for the check anser 
+        //call upon the asnwers 
+        //button click matches the correct answers
+            //it is an if block
+                //if wrong subtract from time interval determied above 
+        //if the current index is the same as the array end the quiz otherwise go to next question 
+    //the ending function 
+        //timer function countdown decrement time and rest time 
+        // goes in start() function
